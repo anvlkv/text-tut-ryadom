36b5ebd2b0cdd94a2c49ea893bbfe0cd8bfda709
a.nvlkv <2813794+anvlkv@users.noreply.github.com>
2024-07-02 22:25:21 +0300

    restore virtual scroll

--- src/lib/taskList.svelte
+++ src/lib/taskList.svelte
@@ -1,47 +1,47 @@
 <script lang="ts">
-  import { page } from "$app/stores";
-  import { getContext, onMount } from "svelte";
-  import InfiniteLoading from "svelte-infinite-loading";
-  import VirtualList from "svelte-tiny-virtual-list";
-  import type { Writable } from "svelte/store";
-  import Task from "./task.svelte";
-  import type { AppData } from "./types/AppData";
+    import { page } from "$app/stores";
+    import { getContext, onMount } from "svelte";
+    import InfiniteLoading from "svelte-infinite-loading";
+    import VirtualList from "svelte-tiny-virtual-list";
+    import type { Writable } from "svelte/store";
+    import Task from "./task.svelte";
+    import type { AppData } from "./types/AppData";
 
-  const ctx: Writable<AppData> = getContext("appData");
-  const taskHeight = 5;
+    const ctx: Writable<AppData> = getContext("appData");
+    const taskHeight = 5.75;
 
-  let asideElement: HTMLElement;
-  let asideHeight = 200;
-  const resize = new ResizeObserver((e) => {
-    asideHeight = e[0].contentRect.height;
-  });
+    let asideElement: HTMLElement;
+    let asideHeight = 200;
+    const resize = new ResizeObserver((e) => {
+        asideHeight = e[0].contentRect.height;
+    });
 
-  let allowScrollTo = false;
-  $: taskIndex = allowScrollTo ? $ctx.entries?.findIndex(
-    (e) => e.input.id === $page.params["task"],
-  ) : undefined;
+    let allowScrollTo = false;
+    $: taskIndex = allowScrollTo
+        ? $ctx.entries?.findIndex((e) => e.input.id === $page.params["task"])
+        : undefined;
 
-  function infiniteHandler({
-    detail: { complete, error },
-  }: CustomEvent<{ complete: () => void; error: () => void }>) {}
+    function infiniteHandler({
+        detail: { complete, error },
+    }: CustomEvent<{ complete: () => void; error: () => void }>) {}
 
-  function onItemsUpdated() {
-    allowScrollTo = true;
-  }
+    function onItemsUpdated() {
+        allowScrollTo = true;
+    }
 
-  onMount(() => {
-    resize.observe(asideElement);
-    return () => {
-      resize.disconnect();
-    };
-  });
+    onMount(() => {
+        resize.observe(asideElement);
+        return () => {
+            resize.disconnect();
+        };
+    });
 </script>
 
 <aside
-  bind:this={asideElement}
-  class="basis-1/5 shrink-0 grow-0 text-sm border-r border-solid border-gray-400/50 dc:border-white lc:border-black overflow-x-hidden h-full"
+    bind:this={asideElement}
+    class="basis-1/5 shrink-0 grow-0 text-sm border-r border-solid border-gray-400/50 dc:border-white lc:border-black overflow-x-hidden h-full"
 >
-  <ul>
+    <!-- <ul>
     {#each ($ctx.entries || []) as entry}
       <li>
           <Task
@@ -50,24 +50,25 @@
         />
       </li>
     {/each}
-  </ul>
-  <!-- <VirtualList
-    height={asideHeight}
-    itemCount={$ctx.total_entries || 0}
-    itemSize={taskHeight * ($ctx.preferences?.fontSize || 16)}
-    on:itemsUpdated={onItemsUpdated}
-    scrollToIndex={taskIndex}
-    scrollToBehaviour="auto"
-    scrollToAlignment="center"
-  >
-    <div slot="item" let:index let:style {style} class="task-entry">
-      <Task
-        task={($ctx.entries || [])[index]}
-        isCurrent={$ctx.current_entry === ($ctx.entries || [])[index].input.id}
-      />
-    </div>
-    <div slot="footer">
-      <InfiniteLoading on:infinite={infiniteHandler} />
-    </div>
-  </VirtualList> -->
+  </ul> -->
+    <VirtualList
+        height={asideHeight}
+        itemCount={$ctx.total_entries || 0}
+        itemSize={taskHeight * ($ctx.preferences?.fontSize || 16)}
+        on:itemsUpdated={onItemsUpdated}
+        scrollToIndex={taskIndex}
+        scrollToBehaviour="auto"
+        scrollToAlignment="center"
+    >
+        <div slot="item" let:index let:style {style} class="task-entry">
+            <Task
+                task={($ctx.entries || [])[index]}
+                isCurrent={$ctx.current_entry ===
+                    ($ctx.entries || [])[index].input.id}
+            />
+        </div>
+        <!-- <div slot="footer">
+            <InfiniteLoading on:infinite={infiniteHandler} />
+        </div> -->
+    </VirtualList>
 </aside>
